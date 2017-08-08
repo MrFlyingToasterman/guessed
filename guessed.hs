c8b9f84c96280cafef5c329eede25d85b6c69ef2
Darius Musiolik <MrFlyingToasterman@yahoo.de>
2017-08-08 09:29:15 +0200

    Added random

--- guessed.hs
+++ guessed.hs
@@ -3,10 +3,19 @@
 --Imports
 import System.IO
 import Data.Char
+import System.Random
 
 -- Main Function (Programm Start)
 main = do
   -- Welcome msg
-  putStrLn("Welcome to Guessed!\n\nI've choosen a Number between 0 and 9. Can you guess it ?\n\n\n")
+  putStrLn("Welcome to Guessed!\n\nI've choosen a Number between 1 and 10. Can you guess it ?\n\n\n")
+  -- Create a Random Int
+  genNumber <- getStdGen
+  let ( randNumber , newGen ) = randomR (1,10) genNumber :: (Int,StdGen)
+  -- Debug: Print Number
+  print randNumber
   -- User Input
+  putStrLn("Your guess: ")
   guess <- getLine
+  -- End expression
+  putStrLn("Ready.")
